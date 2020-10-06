240d458dc6a628f7ef5b9aa298b4a888777dab39
Marcelo Cortes <marcelocortes@pop-os.localdomain>
2020-10-05 23:23:32 -0300

    Lidando com erros

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -19,7 +19,6 @@ appointmentsRouter.get('/', async (request, response) => {
 });
 
 appointmentsRouter.post('/', async (request, response) => {
-    try {
         const { provider_id, date } = request.body;
 
         const parsedDate = parseISO(date);
@@ -29,9 +28,6 @@ appointmentsRouter.post('/', async (request, response) => {
         const appointment = createAppointment.execute({ date: parsedDate, provider_id });
 
         return response.json(appointment)
-    } catch (err) {
-        return response.status(400).json({ error: err.message });
-    }
 });
 
 export default appointmentsRouter;
